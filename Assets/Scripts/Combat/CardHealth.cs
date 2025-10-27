9257817ec80a75f18fd396ce0868e50ee6e044d3
Carlos-Barbio <carlos.barbio2003@gmail.com>
2025-10-27 19:41:46 -0300

    Revive e Cura no final do combate

--- Assets/Scripts/Combat/CardHealth.cs
+++ Assets/Scripts/Combat/CardHealth.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class CardHealth : MonoBehaviour
 {
     public int maxHp = 5;
-    private int hp;
+
+    [SerializeField] private int hp;
+    public int CurrentHp => hp;
+    public bool IsDead => hp <= 0 || !gameObject.activeInHierarchy;
 
     void Awake()
     {
@@ -21,7 +24,20 @@ public class CardHealth : MonoBehaviour
         var combat = GetComponent<CardCombat>();
         if (combat != null) combat.OnDie();
 
-        // desativa visualmente (ou destrói)
+        // desativa visualmente
         gameObject.SetActive(false);
+    }
+
+    // >>> NOVO: Reviver e curar tudo para a próxima rodada
+    public void ReviveAndHeal()
+    {
+        hp = maxHp;
+
+        if (!gameObject.activeSelf)
+            gameObject.SetActive(true);
+
+        var combat = GetComponent<CardCombat>();
+        if (combat != null)
+            combat.IsAlive = true;
     }
 }
